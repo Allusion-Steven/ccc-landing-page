7d9c16a1621e5932f9c91bdcd61e0730b98d97da
Allusion-Steven <steven@allusionsoftware.com>
2025-01-13 21:20:18 -0500

    temp domain until macroexotic is setup

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,4 +3,5 @@
 export const environment = process.env.NODE_ENV;
 export const isLive = (environment === "production") ? true : false
 
-export const baseUrl = isLive ? "https://macroexotics.com" : "http://localhost:5173"
+// export const baseUrl = isLive ? "https://macroexotics.com" : "http://localhost:5173"
+export const baseUrl = isLive ? "https://ccc-landing-page-six.vercel.app" : "http://localhost:5173"
