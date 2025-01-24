b05dd595ea23c429178b300362abad58ae016152
Anthony Horner <61562591+ahorner1117@users.noreply.github.com>
2025-01-24 16:39:57 -0500

    Add TODO comment

--- src/routes/vehicles/+page.svelte
+++ src/routes/vehicles/+page.svelte
@@ -41,6 +41,7 @@
 	}
 </style>
 
+<!-- TODO: Fetch all vehicles from the database -->
 <div class="container mx-auto px-4 py-8">
 	<h1 class="mb-8 text-center text-4xl font-bold text-white">Available Vehicles</h1>
 
