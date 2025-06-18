66f0220aff6a3192595dfd5b10ead642fa42d455
Allusion-Steven <steven@allusionsoftware.com>
2025-06-18 13:37:27 -0400

    mobile style fixes

--- src/lib/components/Banner.svelte
+++ src/lib/components/Banner.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div
-	class="fixed bottom-0 left-0 right-0 z-50 flex items-center justify-center bg-primary-accent/50 p-4 font-[600] leading-tight text-white backdrop-blur-md dark:bg-primary-dark/50 {show
+	class="fixed bottom-0 left-0 right-0 z-50 flex flex-col items-center justify-center bg-primary-accent/50 p-4 font-[600] leading-tight text-white backdrop-blur-md dark:bg-primary-dark/50 {show
 		? 'block'
 		: 'hidden'}">
 	<div class="flex flex-col items-center justify-center gap-1">
@@ -13,7 +13,7 @@
 	</div>
 	<button
 		onclick={() => (show = false)}
-		class="absolute right-4 rounded-md bg-primary-dark/50 px-4 py-2 text-white hover:bg-primary-dark/80 dark:bg-primary-light/80 dark:hover:bg-primary-light/80">
+		class="right-4 mt-4 w-full rounded-md bg-primary-dark/50 px-4 py-2 text-white hover:bg-primary-dark/80 dark:bg-primary-light/80 dark:hover:bg-primary-light/80 sm:absolute sm:mt-0 sm:w-[100px]">
 		{linkText}
 	</button>
 </div>
