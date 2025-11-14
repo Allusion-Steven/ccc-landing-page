78c8f02338a37b095880d198d0753e9f41268db6
Allusion-Steven <steven@allusionsoftware.com>
2025-11-14 12:33:36 -0500

    Update index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,8 @@
 // place files you want to import through the `$lib` alias in this folder.
 
 export const environment = process.env.NODE_ENV;
-// export const isLive = (environment === "production") ? true : false;
-export const isLive = true;
+export const isLive = environment === 'production' ? true : false;
+// export const isLive = true;
 
 // export const baseUrl = isLive ? "https://macroexotics.com" : "http://localhost:5173"
 export const baseUrl = import.meta.env.DEV ? 'http://localhost:5174' : 'https://macroexotics.com';
